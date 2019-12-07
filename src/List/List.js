c12c57305449dc1aa4e89b8097e4e14696d38683
Daniel Di Venere <danieldivenere@gmail.com>
2019-12-07 12:55:04 -0800

    cleaned up

--- src/List/List.js
+++ src/List/List.js
@@ -3,8 +3,7 @@ import Card from '../Card/Card';
 import './List.css';
 
 function List(props){
-    console.log(props.cards);
-    const allCards = props.cards.map(card => <Card title={card.title} content={card.content}></Card>);
+    const allCards = props.cards.map(card => <Card key={card.id} title={card.title} content={card.content}></Card>);
 
     return (
       <section className="List">
